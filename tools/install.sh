3512de5ee4b576b6bab3c947147391c659ab488d
Ricardo Franco <ricardo.krieg@gmail.com>
2021-06-02 09:30:46 -0300

    dont reboot adb

--- tools/install.sh
+++ tools/install.sh
@@ -13,5 +13,3 @@ adb install resources/ADBKeyboard.apk
 adb shell ime set com.android.adbkeyboard/.AdbIME
 adb install resources/instagram.apk
 adb push resources/ig_image.jpeg /sdcard/Download/
-
-adb reboot
